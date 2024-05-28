9c96f71bbe44497886d128d3a1b02d611e921239
M shanmukha nath reddy <nath.shanmukha@gmail.com>
2024-05-28 22:26:55 +0530

    refactoring

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,6 +1,6 @@
 variable "python_code_location" {
   type    = string
-  default = "F:/Course/3-tier-CICD/Terraform"
+  default = "F:/Course/3-tier-CICD/Terraform" # my Terraform code location path /Users/cool/hypha/3tier/3-tier-CICD/Terraform
 }
 variable "static_website_bucket_name" {
   type    = string
