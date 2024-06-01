7528e1e888dc319ee2c7410817110db624b52e6d
M shanmukha nath reddy <nath.shanmukha@gmail.com>
2024-06-01 21:00:51 +0530

    creating infra

--- Terraform/s3.tf
+++ Terraform/s3.tf
@@ -46,4 +46,4 @@ resource "aws_s3_bucket_website_configuration" "static_website_bucket" {
     suffix = "index.html"
   }
 }
-# dummy commit, all ready to go, creating infra
+# dummy commit all good here, ready to create infra
